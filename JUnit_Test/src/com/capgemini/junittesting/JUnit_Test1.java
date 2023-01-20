cda972b5e0e319b60c5d6b4f372c49e1ad545712
Janhavi Kolte <koltejanhavi2001@gmail.com>
2023-01-20 23:19:34 +0530

    first commit

---
+++ JUnit_Test/src/com/capgemini/junittesting/JUnit_Test1.java
@@ -0,0 +1,33 @@
+package com.capgemini.junittesting;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+public class JUnit_Test1 {
+	
+	
+	@Test
+	void display()
+	{
+		System.out.println("Hello All");
+	}
+	
+	
+	@Test
+	void display2()
+	{
+		System.out.println("Hello Shreya");
+	}
+
+	
+	@Test
+	void display3()
+	{
+		System.out.println("Hello Janhavi");
+	}
+
+
+}
---
+++ WildCard/src/com/capgemini/wildcarddemo/practice2.java
@@ -0,0 +1,38 @@
+package com.capgemini.wildcarddemo;
+
+/*public class practice2 {
+	
+	static int a;
+	static {
+		a = 4;
+		System.out.println("Inside sb");
+		System.out.println("a ="+a);
+	}
+	
+	void Test(){
+		System.out.println("inside");
+		a = 10;
+	}
+	
+	public static void func() {
+		a = a +1;
+		System.out.println("a="+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test obj = new Test();
+		obj.func();
+
+	}
+
+}*/
+
+class Main{
+	public static void main(String[] args) {
+		System.out.println(fun());
+	}
+	static int fun() {
+		return 20;
+	}
+}
